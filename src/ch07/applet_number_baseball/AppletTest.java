adae507fb35056f80ab75bc373147d94d0bf82e8
starirof <starirof@naver.com>
2019-08-18 16:48:55 +0900

    start applet

---
+++ src/ch07/applet_number_baseball/AppletTest.java
@@ -0,0 +1,14 @@
+package ch07.applet_number_baseball;
+
+import java.applet.Applet;
+import java.awt.Graphics;
+
+public class AppletTest extends Applet {
+
+	public void paint(Graphics g) {
+		
+		g.drawString("처음 만들어본 자바 애플릿!", 50, 100);
+		
+	}
+	
+}
